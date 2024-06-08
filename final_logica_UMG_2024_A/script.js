753a51718f4ce772e045c948017bd2749adcdfde
PLucero836 <161170178+PLucero836@users.noreply.github.com>
2024-06-08 10:51:26 -0600

    Add files via upload

---
+++ final_logica_UMG_2024_A/script.js
@@ -0,0 +1,12 @@
+function sumar() {
+    varible1 = parseFloat(document.getElementById('variable1').value);
+    varible2 = parseFloat(document.getElementById('variable2').value);
+    varible3 = parseFloat(document.getElementById('variable3').value);
+
+   
+        console.log(varible1, varible2, varible3)
+        resultado =  varible1 + varible2 + variable3 ;
+        document.getElementById('resultado').value = resultado;
+    }
+
+
